533d792d92273f844190045acb6877581d51a109
Aryan-kamal <kamalaryan1234567@gmail.com>
2025-12-09 05:13:25 +0530

    url updated

--- frontend/src/constants.js
+++ frontend/src/constants.js
@@ -1,6 +1,6 @@
-export const API_URL = "http://localhost:3001";
+export const API_URL =
+	"https://sales-management-system-production-46c3.up.railway.app/";
 
 // http://localhost:3001
 
 // https://craftculturebackend-production.up.railway.app
-
