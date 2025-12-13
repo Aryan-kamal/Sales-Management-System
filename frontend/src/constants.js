7fc30b9f4a5e21a05ab776aa78be8a09cd5deeaf
Aryan-kamal <kamalaryan1234567@gmail.com>
2025-12-13 12:48:46 +0530

    backend url updated

--- frontend/src/constants.js
+++ frontend/src/constants.js
@@ -1,6 +1,7 @@
 export const API_URL =
-	"https://sales-management-system-production-0043.up.railway.app/";
+	"https://sales-management-system-production-7a5c.up.railway.app/";
 
 // http://localhost:3001
 
 // https://sales-management-system-production-0043.up.railway.app/
+// sales-management-system-production-7a5c.up.railway.app
